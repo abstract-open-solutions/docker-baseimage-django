b3d02dea0fc91852b5dee6f08a5141e4ce1abaef
Simone Deponti <simone.deponti@abstract.it>
2016-05-06 18:45:31 +0200

    Using exec to completely replace the shell.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,19 +10,19 @@ args=("$@")
 case $1 in
     manage)
         export DJANGO_SETTINGS_MODULE=conf.production
-        bin/python manage.py ${args[@]:1}
+        exec bin/python manage.py ${args[@]:1}
         ;;
     run)
         export DJANGO_SETTINGS_MODULE=conf.production
-        bin/gunicorn conf.wsgi -b 0.0.0.0:8000 -w 4
+        exec bin/gunicorn conf.wsgi -b 0.0.0.0:8000 -w 4
         ;;
     develop-manage)
         export DJANGO_SETTINGS_MODULE=conf.development
-        bin/python manage.py ${args[@]:1}
+        exec bin/python manage.py ${args[@]:1}
         ;;
     develop-run)
         export DJANGO_SETTINGS_MODULE=conf.development
-        bin/python manage.py runserver 0.0.0.0:8000
+        exec bin/python manage.py runserver 0.0.0.0:8000
         ;;
     *)
         if [ -f ./entrypoint-extras.sh ]; then
