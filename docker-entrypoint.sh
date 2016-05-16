b29541767ab04fd9015a57605c9aa107f05f894f
Simone Deponti <simone.deponti@gmail.com>
2016-05-16 16:33:34 +0200

    Fix execution of extra endpoints.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -26,7 +26,7 @@ case $1 in
         ;;
     *)
         if [ -f ./entrypoint-extras.sh ]; then
-            ./entrypoint-extras.sh "$@"
+            ./entrypoint-extras.sh ${args[@]}
         else
             exec "$@"
         fi
